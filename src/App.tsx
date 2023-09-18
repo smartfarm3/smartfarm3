246aaadb424ff4a426c22fe157ee89fff3ee564b
park <1998psw@korea.ac.kr>
2023-09-18 20:07:53 +0900

    헤더랑 푸터 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import "../public/css/index.css";
 
-import Home from "./pages/Page_Home"
-import Sub from "./pages/Page_Sub";
+import Header from "./pages/Header";
+// import Home from "./pages/Page_Home";
+// import Sub from "./pages/Page_Sub";
+// import Footer from "./pages/Footer";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/world" element={<Sub />}></Route>
-            </Routes>
-        </BrowserRouter>
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route path="/" element={<Home />}></Route>
+        //         <Route path="/world" element={<Sub />}></Route>
+        //     </Routes>
+        // </BrowserRouter>
+        <Header />
     );
 }
 
---
+++ src/pages/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import tw, { css } from "twin.macro";
+
+const Header = () => {
+    return (
+        <div className="lg:w-full w-full bg-blue-0">
+            123
+        
+        </div>
+    );
+};
+
+export default Header;
+
+
+
