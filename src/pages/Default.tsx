8597c344e43f6cbcdb82a818259b5925ebc39d6f
swpark <1998psw@korea.ac.kr>
2023-09-21 11:54:04 +0900

    123

--- src/pages/Default.tsx
+++ src/pages/Default.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 const Default = () => {
     return (
         <div className="">
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 // import tw, { css } from "twin.macro";
 import Robottok from "../imagesfarm/Header/Robottok.png";
 
